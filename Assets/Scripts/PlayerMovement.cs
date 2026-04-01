36ea9ea47119c534afaa5d1dc824d14454111360
MateuszGolebiewski591 <m.golebiewski591@gmail.com>
2026-04-01 14:09:15 +0100

    Added and improved upon wall sticking. Switched to switch-based player state machine and adjusted how the jump works.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,21 +13,31 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Parameters")]
     private Vector2 horizontalVector = Vector2.zero;
     private Vector2 verticalVector = Vector2.zero;
+    private Vector2 groundSurface =  Vector2.zero;
     [SerializeField] float groundMovementSpeed = 7.5f;
     [SerializeField] float airMovementSpeed = 10f;
     private float movementSpeed = 0f;
-    [SerializeField] private Vector2 gravity = -Vector2.up;
+    [SerializeField] private Vector2 gravity = Vector2.down;
     [SerializeField] float gravityForce = 9.81f; 
     [SerializeField] float terminalVelocity = 10f;
     private bool jumping = false;
     private bool jumpButtonDown = false;
-    private bool jumpAvalailable = false;
+    private bool jumpAvalailable = true;
     private float jumpTimeElapsed = 0f;
+    private bool takingOff = false;
     [SerializeField] float maxJumpTime = 2f;
     [SerializeField] float jumpScalar = 100f;
     private Coroutine jumpTimer;
     [SerializeField] private AnimationCurve jumpCurve;
     [SerializeField] private float groundCheckDistance = 0.1f;
+    private PlayerState state = PlayerState.Falling;
+
+    private enum PlayerState
+    {
+        Grounded,
+        Jumping,
+        Falling,
+    }
 
 
     void Awake()
@@ -40,8 +50,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!IsOnSurface()) gravity = Vector2.down;
-        if (jumping && jumpButtonDown) verticalVector = Vector2.up * jumpScalar * jumpCurve.Evaluate(jumpTimeElapsed);
+        //if (!IsOnSurface()) gravity = Vector2.down;
+        /*if (jumping && jumpButtonDown) verticalVector = Vector2.up * jumpScalar * jumpCurve.Evaluate(jumpTimeElapsed);
         else if (!IsGrounded()) verticalVector += gravity * gravityForce;
         else verticalVector = Vector2.zero;
         if (verticalVector.y < -terminalVelocity) verticalVector = Vector2.ClampMagnitude(verticalVector, terminalVelocity);
@@ -53,8 +63,86 @@ public class PlayerMovement : MonoBehaviour
         else {
             jumpAvalailable = false;
             movementSpeed = airMovementSpeed;
+        }*/
+        
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void SetState(PlayerState newState)
+    {
+        switch (newState)
+        {
+            case PlayerState.Grounded:
+                {
+                    //if (state == PlayerState.Jumping) return;
+                    state = PlayerState.Grounded;
+                    jumpAvalailable = true;
+                    break;
+                }
+            case PlayerState.Jumping:
+                {
+                    state = PlayerState.Jumping;
+                    jumpAvalailable = false;
+                    break;
+                }
+            case PlayerState.Falling:
+                {
+                    state = PlayerState.Falling;
+                    jumpAvalailable = false;
+                    break;
+                }
+        }
+    }
+
+    void HandleMovement()
+    {
+        bool grounded = IsOnSurface();
+        if (grounded && !takingOff) {
+            if (Mathf.Abs(Vector2.Dot(gravity, groundSurface)) > 0.1) SetState(PlayerState.Falling);
+            else SetState(PlayerState.Grounded);
+        }
+        if (takingOff && !grounded) takingOff = false;
+
+        switch (state)
+        {
+            case PlayerState.Grounded:
+                {
+                    if (!grounded && !takingOff) {
+                        SetState(PlayerState.Falling);
+                        jumpAvalailable = false;
+                        break;
+                    }
+                    verticalVector = Vector2.zero;
+                    break;
+                }
+            case PlayerState.Jumping:
+                {
+                    verticalVector = -gravity;
+                    break;
+                }
+            case PlayerState.Falling:
+                {
+                    verticalVector = Vector2.down;
+                    break;
+                }
         }
         
+        /*if (grounded && !jumpButtonDown) {
+            verticalVector = Vector2.zero;
+            jumpAvalailable = true;
+        }
+        else if (!grounded && !jumpButtonDown) verticalVector = Vector2.down;
+        else verticalVector = gravity;
+        if (jumpButtonDown)
+        {   
+            verticalVector = -gravity;
+        }*/
+        verticalVector *= gravityForce;
+        rb.linearVelocity = verticalVector + horizontalVector;
     }
 
 
@@ -83,6 +171,8 @@ public class PlayerMovement : MonoBehaviour
                     foundSurface = true;
                     closestSide = raycastHit.distance;
                     gravity = normal;
+                    //gravity = raycastHit.normal;
+                    groundSurface = new Vector2(raycastHit.normal.y, -raycastHit.normal.x);
                 }
             }
         }
@@ -99,13 +189,13 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.started && jumpAvalailable)
         {
-            jumpButtonDown = true;
-            jumpAvalailable = false;
-            jumpTimer = StartCoroutine(JumpTimer());
+            takingOff = true;
+            SetState(PlayerState.Jumping);
+            //jumpTimer = StartCoroutine(JumpTimer());
         }
         if (context.canceled)
         {
-            jumpButtonDown = false;
+            SetState(PlayerState.Falling);
         }
     }
 
